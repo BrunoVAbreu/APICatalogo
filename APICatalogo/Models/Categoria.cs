7f8e160ae7e98c60b259d7e4d6c571e33e2b952d
Bruno Abreu <vazs.bruno@gmail.com>
2026-03-16 22:12:38 -0300

    Criando o modelo de dominio adicionando as classes Categoria e Produto

---
+++ APICatalogo/Models/Categoria.cs
@@ -0,0 +1,10 @@
+﻿namespace APICatalogo.Models;
+
+public class Categoria
+{
+    public int CategoriaID{ get; set; }
+    public String? Nome { get; set; }
+
+    public String? ImageURL { get; set; }
+
+}
---
+++ APICatalogo/Models/Produto.cs
@@ -0,0 +1,13 @@
+﻿namespace APICatalogo.Models
+{
+    public class Produto
+    {
+        public int ProdutoID { get; set; }
+        public String? Nome { get; set; }
+        public String? Descricao { get; set; }
+        public decimal? Preco { get; set; }
+        public string? ImageURL { get; set; }
+        public float Estoque { get; set; }
+        public DateTime DatCadastro { get; set; }
+    }
+}
