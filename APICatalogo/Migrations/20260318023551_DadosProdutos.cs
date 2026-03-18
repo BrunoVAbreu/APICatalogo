760572d4b17b773995e157fe4914ffb0cb306abf
Bruno Abreu <vazs.bruno@gmail.com>
2026-03-17 23:42:25 -0300

    Aplicando o  Data Annotations e populando as tabelas com dados iniciais

---
+++ APICatalogo/Migrations/20260318023551_DadosProdutos.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class DadosProdutos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert into Produtos(Nome,Descricao,Preco,ImageUrl,Estoque,DataCadastro,CategoriaId) Values('Coca-Cola Diet','Refrigerante de Cola 350 ml',5.45,'cocacola.jpg',50,now(),9)");
+
+            mb.Sql("Insert into Produtos(Nome,Descricao,Preco,ImageUrl,Estoque,DataCadastro,CategoriaId) Values('Lanche de Atum','Lanche de Atum com maionese',8.50,'atum.jpg',10,now(),10)");
+
+            mb.Sql("Insert into Produtos(Nome,Descricao,Preco,ImageUrl,Estoque,DataCadastro,CategoriaId) Values('Pudim 100 g','Pudim de leite condensado 100g',6.75,'pudim.jpg',20,now(),11)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Produtos");
+
+        }
+    }
+}
--- APICatalogo/Models/Produto.cs
+++ APICatalogo/Models/Produto.cs
@@ -1,14 +1,29 @@
-﻿namespace APICatalogo.Models
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace APICatalogo.Models
 {
+    [Table("Produtos")]
     public class Produto
     {
+        [Key]
         public int ProdutoID { get; set; }
+        [Required]
+        [StringLength(80)]
         public String? Nome { get; set; }
+        [Required]
+        [StringLength(300)]
         public String? Descricao { get; set; }
+        [Required]
+        [Column(TypeName = "decimal(10,2)")]
         public decimal? Preco { get; set; }
+        [Required]
+        [StringLength(300)]
         public string? ImageURL { get; set; }
         public float Estoque { get; set; }
-        public DateTime DatCadastro { get; set; }
+        public DateTime DataCadastro { get; set; }
 
         public int CategoriaID { get; set; }
         public Categoria? Categoria { get; set; }
